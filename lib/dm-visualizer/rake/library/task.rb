64673b79fc83f8a332ab523dacb34ac05fe763d3
postmodern <postmodern.mod3@gmail.com>
2010-04-25 19:25:36 -0700

    Added a basic GraphViz Rake Task.

---
+++ lib/dm-visualizer/rake/library/task.rb
@@ -0,0 +1,44 @@
+require 'rake'
+
+module DataMapper
+  module Visualizer
+    module Rake
+      module Library
+        class Task < Rake::TaskLib
+          # The default options to pass to a new {Visualization}
+          DEFAULT_OPTIONS = {
+            :include => ['ext', 'lib'],
+            :bundle => [:runtime]
+          }
+
+          #
+          # Creates a new task.
+          #
+          # @yield [task]
+          #   The given block will be passed the newly created task.
+          #   
+          # @yieldparam [Task] task
+          #   The new Task.
+          #
+          def initialize(options={})
+            yield self if block_given?
+
+            define()
+          end
+
+          #
+          # Defines a task within the `dm:doc` namespace.
+          #
+          def define(&block)
+            namespace :dm do
+              namespace :doc do
+                instance_eval(&block) if block
+              end
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
