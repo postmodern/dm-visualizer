96c735850d5dc187da7fd91137289513815f01a8
Postmodern <postmodern.mod3@gmail.com>
2012-08-03 19:40:04 -0700

    Fixed Visualization specs.

--- spec/visualization_spec.rb
+++ spec/visualization_spec.rb
@@ -1,88 +1,76 @@
-require 'dm-visualizer/visualization'
-
 require 'spec_helper'
 require 'helpers/project'
 
-describe DataMapper::Visualizer::Visualization do
-  describe "defaults" do
-    before(:all) do
-      @vis = DataMapper::Visualizer::Visualization.new(
-        :include => [File.join(project_dir('library'),'lib')],
-        :require => ['blog']
-      )
-    end
+require 'dm-visualizer/visualization'
 
+describe DataMapper::Visualizer::Visualization do
+  let(:lib_dir) { File.join(project_dir('library'),'lib') }
+
+  before(:all) do
+    $LOAD_PATH << lib_dir
+    require 'blog'
+  end
+
+  context "defaults" do
     it "should return the class names of Classes" do
-      @vis.class_name(Blog::User).should == 'User'
+      subject.class_name(Blog::User).should == 'User'
     end
 
     it "should return the class names of Objects" do
-      @vis.class_name(Blog::Post.body).should == 'Text'
+      subject.class_name(Blog::Post.body).should == 'Text'
     end
 
     it "should return the names of properties" do
-      @vis.property_name(Blog::Post.body).should == 'body'
+      subject.property_name(Blog::Post.body).should == 'body'
     end
 
     it "should return the names of models" do
-      @vis.model_name(Blog::User).should == 'User'
+      subject.model_name(Blog::User).should == 'User'
     end
   end
 
-  describe "repository names" do
-    before(:all) do
-      @vis = DataMapper::Visualizer::Visualization.new(
-        :include => [File.join(project_dir('library'),'lib')],
-        :require => ['blog'],
-        :repository_names => {:default => 'blogdb'}
-      )
-    end
+  context "with repository names" do
+    subject { described_class.new(:repository_names => {:default => 'blogdb'}) }
 
     it "should map the DataMapper repository names" do
-      @vis.model_repository_name(Blog::User).should == 'blogdb'
+      subject.model_repository_name(Blog::User).should == 'blogdb'
     end
   end
 
-  describe "Schema naming convention" do
-    before(:all) do
-      @vis = DataMapper::Visualizer::Visualization.new(
-        :include => [File.join(project_dir('library'),'lib')],
-        :require => ['blog'],
+  context "with schema naming convention" do
+    subject do
+      described_class.new(
         :repository_names => {:default => 'blogdb'},
-        :naming => :schema
+        :naming           => :schema
       )
     end
 
     it "should return the database and table names for a model name" do
-      @vis.model_name(Blog::User).should == 'blogdb.blog_users'
+      subject.model_name(Blog::User).should == 'blogdb.blog_users'
     end
   end
 
-  describe "full names" do
-    before(:all) do
-      @vis = DataMapper::Visualizer::Visualization.new(
-        :include => [File.join(project_dir('library'),'lib')],
-        :require => ['blog'],
-        :full_names => true
-      )
-    end
+  context "with full names" do
+    subject { described_class.new(:full_names => true) }
 
     it "should not demodulize the names of Classes" do
-      @vis.class_name(Blog::User).should == 'Blog::User'
+      subject.class_name(Blog::User).should == 'Blog::User'
     end
 
     it "should not demodulize the names of Objects" do
       object = Blog::Post.new
 
-      @vis.class_name(object).should == 'Blog::Post'
+      subject.class_name(object).should == 'Blog::Post'
     end
 
     it "should not demodulize property type names" do
-      @vis.property_type_name(Blog::Post.body).should =~ /::Text$/
+      subject.property_type_name(Blog::Post.body).should =~ /::Text$/
     end
 
     it "should not demodulize model names" do
-      @vis.model_name(Blog::User).should == 'Blog::User'
+      subject.model_name(Blog::User).should == 'Blog::User'
     end
   end
+
+  after(:all) { $LOAD_PATH.delete(lib_dir) }
 end
